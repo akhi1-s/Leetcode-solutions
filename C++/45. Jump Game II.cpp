0c36fcb4dcb1c9531891413a42231adef40a76ea
Ꭿkhil <theakhil2000@gmail.com>
2025-01-27 15:30:58 +0530

    Update 45. Jump Game II.cpp

--- C++/45. Jump Game II.cpp
+++ C++/45. Jump Game II.cpp
@@ -1,23 +1,24 @@
-// Time:  O(n)
-// Space: O(n)
 
 class Solution {
-public:
-    bool canReach(vector<int>& arr, int start) {
-        queue<int> q({start});
-        unordered_set<int> lookup = {start};
-        while (!q.empty()) {
-            const auto i = q.front(); q.pop();
-            if (!arr[i]) {
-                return true;
-            }
-            for (const auto& j : {i - arr[i], i + arr[i]}) {
-                if (0 <= j && j < arr.size() && !lookup.count(j)) {
-                    lookup.emplace(j);
-                    q.emplace(j);
-                }
-            }
-        }
-        return false;
+ public:
+  int jump(vector<int>& nums) {
+    int ans = 0;
+    int end = 0;
+    int farthest = 0;
+
+    // Start an implicit BFS.
+    for (int i = 0; i < nums.size() - 1; ++i) {
+      farthest = max(farthest, i + nums[i]);
+      if (farthest >= nums.size() - 1) {
+        ++ans;
+        break;
+      }
+      if (i == end) {    // Visited all the items on the current level.
+        ++ans;           // Increment the level.
+        end = farthest;  // Make the queue size for the next level.
+      }
     }
+
+    return ans;
+  }
 };

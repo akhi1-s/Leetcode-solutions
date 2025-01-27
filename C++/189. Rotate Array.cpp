66a34ce9560b25d6bbba99f14dae2eb5ed3d1e7f
Ꭿkhil <theakhil2000@gmail.com>
2025-01-27 16:12:40 +0530

    Update 189. Rotate Array.cpp

--- C++/189. Rotate Array.cpp
+++ C++/189. Rotate Array.cpp
@@ -1,23 +1,14 @@
+// Time:  O(n)
+// Space: O(1)
+
 class Solution {
- public:
-  int longestUnivaluePath(TreeNode* root) {
-    int ans = 0;
-    longestUnivaluePathDownFrom(root, ans);
-    return ans;
-  }
-
- private:
-  int longestUnivaluePathDownFrom(TreeNode* root, int& ans) {
-    if (root == nullptr)
-      return 0;
-
-    const int l = longestUnivaluePathDownFrom(root->left, ans);
-    const int r = longestUnivaluePathDownFrom(root->right, ans);
-    const int arrowLeft =
-        root->left && root->left->val == root->val ? l + 1 : 0;
-    const int arrowRight =
-        root->right && root->right->val == root->val ? r + 1 : 0;
-    ans = max(ans, arrowLeft + arrowRight);
-    return max(arrowLeft, arrowRight);
-  }
+public:
+    void rotate(vector<int>& nums, int k) {
+        if (!nums.empty()) {
+            k %= nums.size();
+            reverse(nums.begin(), nums.begin() + nums.size() - k);
+            reverse(nums.begin() + nums.size() - k, nums.end());
+            reverse(nums.begin(), nums.end());
+        }
+    }
 };

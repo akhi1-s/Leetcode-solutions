b7b2933001f00962be3c24f5a8cdb7bf943f5b7a
Ꭿkhil <theakhil2000@gmail.com>
2025-02-02 15:49:39 +0530

    Create 1894. Find the Student that Will Replace the Chalk.cpp

---
+++ C++/1894. Find the Student that Will Replace the Chalk.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        long long s = accumulate(chalk.begin(), chalk.end(), 0LL);
+        k %= s;
+        for (int i = 0;; ++i) {
+            if (k < chalk[i]) {
+                return i;
+            }
+            k -= chalk[i];
+        }
+    }
+};

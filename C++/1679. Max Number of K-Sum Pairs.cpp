e7dc7bf196bfd9543eff0ada8869296308a77f65
Ꭿkhil <theakhil2000@gmail.com>
2025-02-03 21:42:25 +0530

    Create 1679. Max Number of K-Sum Pairs.cpp

---
+++ C++/1679. Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int, int> cnt;
+        int ans = 0;
+        for (int& x : nums) {
+            if (cnt[k - x]) {
+                --cnt[k - x];
+                ++ans;
+            } else {
+                ++cnt[x];
+            }
+        }
+        return ans;
+    }
+};

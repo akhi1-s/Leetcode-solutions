7abc7f6c4fb0178602e7ad48ecd7388ce4f139b3
Ꭿkhil <theakhil2000@gmail.com>
2025-02-07 22:21:59 +0530

    Create 137. Single Number II.cpp

---
+++ C++/137. Single Number II.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int a = 0, b = 0;
+        for (int c : nums) {
+            int aa = (~a & b & c) | (a & ~b & ~c);
+            int bb = ~a & (b ^ c);
+            a = aa;
+            b = bb;
+        }
+        return b;
+    }
+};

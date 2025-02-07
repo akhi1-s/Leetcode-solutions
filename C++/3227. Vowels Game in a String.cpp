a64c6c0d41ea44c2168b3614c812efc7cab2804b
Ꭿkhil <theakhil2000@gmail.com>
2025-02-07 14:18:08 +0530

    Create 3227. Vowels Game in a String.cpp

---
+++ C++/3227. Vowels Game in a String.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        string vowels = "aeiou";
+        for (char c : s) {
+            if (vowels.find(c) != string::npos) {
+                return true;
+            }
+        }
+        return false;
+    }
+};

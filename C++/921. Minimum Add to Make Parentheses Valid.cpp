84365cf416d7167d4619be5b03301e6e6c3d5a09
Ꭿkhil <theakhil2000@gmail.com>
2025-02-10 12:08:41 +0530

    Create 921. Minimum Add to Make Parentheses Valid.cpp

---
+++ C++/921. Minimum Add to Make Parentheses Valid.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int ans = 0, cnt = 0;
+        for (char c : s) {
+            if (c == '(')
+                ++cnt;
+            else if (cnt)
+                --cnt;
+            else
+                ++ans;
+        }
+        ans += cnt;
+        return ans;
+    }
+};

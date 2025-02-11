d8eaeabf8efd48f1ffa14b93f66d99366c4ca08f
Ꭿkhil <theakhil2000@gmail.com>
2025-02-11 10:29:03 +0530

    Create 1817. Finding the Users Active Minutes.cpp

---
+++ c++2/1817. Finding the Users Active Minutes.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+        unordered_map<int, unordered_set<int>> d;
+        for (auto& log : logs) {
+            int i = log[0], t = log[1];
+            d[i].insert(t);
+        }
+        vector<int> ans(k);
+        for (auto& [_, ts] : d) {
+            ++ans[ts.size() - 1];
+        }
+        return ans;
+    }
+};
